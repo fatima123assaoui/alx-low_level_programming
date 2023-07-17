a05780ba689bc0d4108568d1a5fad0d7b83ca60d
Fatima Assaoui <fatima.assaoui.19@gmail.com>
2023-07-16 17:46:11 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry points
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}		
