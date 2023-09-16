b2232a92147d5f109e9546f51458be056080fe3e
root <fatima.assaoui.19@gmail.com>
2023-09-16 13:35:20 -0700

    f

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
